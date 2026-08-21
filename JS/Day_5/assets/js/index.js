e81b50da1bc2585f9f8e0c3e7521df3110c7d013
Thamizhselvan7584 <tamilselven199@gmail.com>
2026-08-22 00:07:38 +0530

    js Day 5 completed

---
+++ JS/Day_5/assets/js/index.js
@@ -0,0 +1,89 @@
+
+/* task 1 */
+
+for (let a = 0; a <= 10; a++) {
+    console.log(a);
+
+}
+
+/* Task 2 */
+
+for (let a = 10; a >= 0; a--) {
+    console.log(a);
+}
+
+
+/* Task 3 */
+
+for (let a = 0; a <= 20; a++) {
+    console.log(a);
+
+}
+
+
+/* Task 4 */
+
+let a = 10;
+for (a; a > 0; a--) {
+    if (a % 2 === 0) {
+        console.log(a);
+    }
+}
+
+/* Task 5 */
+
+let b = 10;
+for (b; b >= 0; b--) {
+    if (b % 2 === 1) {
+        console.log(b);
+    }
+}
+
+
+/* TASK 6    PRINT NUMBERS */
+
+for (let a = 1; a <= 10; a++) {
+    console.log(a);
+
+}
+
+/* TASK 7    EVEN NUMBERS */
+
+for (let a = 2; a <= 20; a += 2) {
+    console.log(a);
+
+}
+
+/* TASK 8    ODD NUMBERS */
+
+for (let a = 1; a <= 20; a += 2) {
+    console.log(a);
+
+}
+
+/* Task 9   MULTIPLICATION TABLE */
+
+
+let n = 5;
+
+for (let a = 1; a <= 10; a++) {
+
+    console.log(a + "X" + n + "=" + a * n);
+}
+
+/* Task 10    SUM OF NUMBERS*/
+
+
+let sum = 0;
+for (let a = 1; a <= 10; a++) {
+    sum = a + sum;
+
+    console.log(sum);
+}
+
+/* Task 6 : print the Number */
+
+for (let a = 10; a >= 5; a--) {
+    console.log(a);
+
+}
